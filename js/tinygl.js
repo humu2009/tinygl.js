8ce7ae5339fab8fd1e3b4171a17d7aae1f9e6e2a
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-11 09:42:30 -0800

    Update tinygl.js

--- js/tinygl.js
+++ js/tinygl.js
@@ -53,6 +53,8 @@
  * var TinyGLRenderingContext;
  */
 
+var DEBUG = false;
+
 function initializeTinyGLRuntime(options) {
 	// define a console for message output
 	var debug_output = (typeof console) != 'undefined' ? console : {
@@ -2284,8 +2286,18 @@ setTimeout(function(){setTimeout(function(){r.setStatus("")},1);ja||b()},1)):b()
   
           swapBuffers: function() {
               var frame_buf_size = this._frame_buf_width * this._frame_buf_height * BYTES_PER_UINT32;
+	      var start;
+	      if(DEBUG)
+		      start = Date.now();
               var frame_buf = Module.HEAPU8.subarray(this._frame_buf_ptr, this._frame_buf_ptr + frame_buf_size);
+	      if(DEBUG)
+		      console.log('Subarray took ' + (Date.now() - start).toString() + 'ms');
+              if(DEBUG)
+		      start = Date.now();
               this._driver.deliver(this._vp, frame_buf);
+	      if(DEBUG)
+		  console.log('Deliver took ' + (Date.now() - start).toString() + 'ms');
+		
           }
   
       };
